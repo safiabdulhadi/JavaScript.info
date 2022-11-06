59edc47566ced0b8709c5935bfe38fe14e2ebfd7
safiabdulhadi1994 <safiabdulhadi1994@gmail.com>
2022-11-06 15:05:07 +0100

    dataTypes

--- DataTypes/script.js
+++ DataTypes/script.js
@@ -1,26 +1,42 @@
-'use strict';
-let message = 'hello';
-// message = 123456;
-console.log(message);
-let n = 123;
-n = 12.345;
-console.log(n);
+// 'use strict';
+// let message = 'hello';
+// // message = 123456;
+// console.log(message);
+// let n = 123;
+// n = 12.345;
+// console.log(n);
 
-let str = 'Hello';
+// let str = 'Hello';
+// let str2 = 'Single quotes are ok too';
+// let phrase = `can embed another ${str}`;
+
+// console.log(phrase);
+
+// let name = 'John';
+
+// // embed a variable
+// alert(`Hello, ${name}!`); // Hello,John!
+
+// // embed an expression
+// alert(`the result is ${1 + 2}`); // the result is 3
+
+// let isGreater = 4 > 1;
+
+// alert(isGreater); // true (the comparison result is "yes")
+// console.log(isGreater); // result is true
+
+// In JavaScript, there are 3 types of quotes.
+
+// Double quotes: "Hello".
+// Single quotes: 'Hello'.
+// Backticks: `Hello`.
+// Double and single quotes are “simple” quotes. There’s practically no difference between them in JavaScript.
+
+// Backticks are “extended functionality” quotes. They allow us to embed variables and expressions into a string by wrapping them in ${…}, for example:
+let str = "Hello";
 let str2 = 'Single quotes are ok too';
 let phrase = `can embed another ${str}`;
-
+console.log(str);
+console.log(str2);
 console.log(phrase);
 
-let name = 'John';
-
-// embed a variable
-alert(`Hello, ${name}!`); // Hello,John!
-
-// embed an expression
-alert(`the result is ${1 + 2}`); // the result is 3
-
-let isGreater = 4 > 1;
-
-alert(isGreater); // true (the comparison result is "yes")
-console.log(isGreater); // result is true
--- TypeConversions/script.js
+++ TypeConversions/script.js
@@ -1,5 +1,9 @@
-let value = true;
-alert(typeof value); // boolean
+// let value = true;
+// alert(typeof value); // boolean
 
-value = String(value); // now value is a string "true"
-alert(typeof value); // string
+// value = String(value); // now value is a string "true"
+// alert(typeof value); // string
+
+
+
+
