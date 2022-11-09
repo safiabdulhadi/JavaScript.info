812f9ab3adc143a296fc02dc00be55a33d0fb90f
safiabdulhadi1994 <safiabdulhadi1994@gmail.com>
2022-11-09 10:13:56 +0100

    Concate & backtick

---
+++ fundamentals/stringConcatenation/script.js
@@ -0,0 +1,13 @@
+// let name = 'Abdulhadi';
+// let lastName = 'Safi';
+// let fullName = name + ' ' + lastName;
+// console.log(' Hello there your full name is :' + fullName);
+// the out is Hello there your full name is : Abdulhadi Safi
+
+
+let name = 'Abdulhadi';
+let lastName = 'Safi';
+let fullName = `${name} ${lastName}`;
+console.log( `Hello there your full name is : ${fullName}`);
+// the out is Hello there your full name is : Abdulhadi Safi
+
