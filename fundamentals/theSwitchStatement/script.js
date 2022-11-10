7fbc3f7bac8a68d0e5f2bde18c20dfcc69ed09f1
safiabdulhadi1994 <safiabdulhadi1994@gmail.com>
2022-11-10 15:38:08 +0100

    switch statement

--- fundamentals/theSwitchStatement/script.js
+++ fundamentals/theSwitchStatement/script.js
@@ -1,14 +1,31 @@
-let a = 2 + 3;
-switch(a){
-    case 1:
-    console.log("Too small number");
-    break;
-    case 4:
-    console.log("Exactly number");
-    break;
-    case 5:
-        console.log("Too big number");
+// let a = 2 + 3;
+// switch(a){
+//     case 1:
+//     console.log("Too small number");
+//     break;
+//     case 4:
+//     console.log("Exactly number");
+//     break;
+//     case 5:
+//         console.log("Too big number");
+//         break;
+//         default:
+//             console.log("i don't know such values sorry");
+// }
+
+let browser = 'Chrome';
+
+
+switch(browser){
+    case 'Chrome':
+        console.log("Chrome");
+        break;
+        case 'Safari':
+        console.log("Safari");
+        break;
+        case 'Opera':
+        console.log("Opera");
         break;
         default:
-            console.log("i don't know such values sorry");
+            console.log("i don't know");
 }
