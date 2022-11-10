634e759a79d78b8f6e7520310719d31b5496fde6
safiabdulhadi1994 <safiabdulhadi1994@gmail.com>
2022-11-10 15:48:04 +0100

    switch

--- fundamentals/theSwitchStatement/script.js
+++ fundamentals/theSwitchStatement/script.js
@@ -13,19 +13,37 @@
 //             console.log("i don't know such values sorry");
 // }
 
-let browser = 'Chrome';
+// let browser = 'Chrome';
 
 
-switch(browser){
-    case 'Chrome':
-        console.log("Chrome");
-        break;
-        case 'Safari':
-        console.log("Safari");
-        break;
-        case 'Opera':
-        console.log("Opera");
-        break;
-        default:
-            console.log("i don't know");
-}
+// switch(browser){
+//     case 'Chrome':
+//         console.log("Chrome");
+//         break;
+//         case 'Safari':
+//         console.log("Safari");
+//         break;
+//         case 'Opera':
+//         console.log("Opera");
+//         break;
+//         default:
+//             console.log("i don't know");
+// }
+
+/*
+Pour correspondre précisément à la fonctionnalité switch, le if doit utiliser
+une comparaison stricte '==='.
+
+Cependant, pour des chaînes de caractères données, un simple '==' fonctionne également.
+*/
+// let browser = 'Chrome';
+
+// if(browser == 'Edge'){
+//     console.log("Edge");
+// }else if(browser == 'Firefox'){
+//     console.log("firefox");
+// } else if(browser == 'Chrome'){
+//     console.log("Chrome");
+// }else{
+//     console.log("i don't know");
+// }
