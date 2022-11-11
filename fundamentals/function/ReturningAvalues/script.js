02fdb7693d22a10d24678eda96ea76e84ed1b78b
safiabdulhadi1994 <safiabdulhadi1994@gmail.com>
2022-11-11 23:25:24 +0100

    return a values

---
+++ fundamentals/function/ReturningAvalues/script.js
@@ -0,0 +1,30 @@
+/*
+Returning a Values
+A function can return a value back into the calling code as the rsult.
+
+The simplest example would e a function that sums two values
+*/
+
+// function sum(a, b){
+//     return a + b;
+// }
+// let result = sum(2,2);
+
+// console.log(result);
+
+// function checkAge(age){
+// if(age => 18){
+//     return true;
+// }else{
+//     confirm('Do you have permission from your parents ?');
+// }
+// }
+
+// let age = prompt('How old are you ?', 18);
+
+// if(checkAge (age)){
+//     console.log('Access granted');
+// }else{
+//     console.log('Access denied');
+// }
+
