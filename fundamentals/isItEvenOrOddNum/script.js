4d1576e51d1f9b39e67199063c640574a9f34eba
safiabdulhadi1994 <safiabdulhadi1994@gmail.com>
2022-11-12 10:37:16 +0100

    fun for even or odd num

---
+++ fundamentals/isItEvenOrOddNum/script.js
@@ -0,0 +1,11 @@
+function isItEvenOrOddNum(numbers){
+    if(numbers % 2 === 0){
+      return ("This is Even numbers");
+    }else{
+      return ("this is Odd numbers");
+    }
+  }
+//   console.log(isItEvenOrOddNum());
+  console.log(isItEvenOrOddNum(0));
+  console.log(isItEvenOrOddNum(1));
+  console.log(isItEvenOrOddNum(2));
