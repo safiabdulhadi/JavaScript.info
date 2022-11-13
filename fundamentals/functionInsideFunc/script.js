d28e4fdb689f787f8941cafd08bb4d732e82e57b
safiabdulhadi1994 <safiabdulhadi1994@gmail.com>
2022-11-13 22:01:40 +0100

    function inside the function

---
+++ fundamentals/functionInsideFunc/script.js
@@ -0,0 +1,21 @@
+// function app(){
+// console.log("hello app function");
+
+// const myFunction = () =>{
+//     console.log( "myFunction inside the function");
+// }
+// myFunction(); // it work
+// const addTwoNumbers = (num1,num2) =>{
+//     return num1 + num2;
+
+// }
+// console.log(addTwoNumbers(20,30)); // 50
+
+// const multiple =(num3 , num4) => num3 * num4;
+//  ;
+
+// resultOfMultiple = multiple(5 , 8); // 40
+// console.log(resultOfMultiple);
+
+// }
+// app()
