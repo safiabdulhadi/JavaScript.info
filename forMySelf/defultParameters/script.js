1f3a5de96ac0bc2be9f94e19afad08f29add3b25
safiabdulhadi1994 <safiabdulhadi1994@gmail.com>
2022-11-14 09:57:37 +0100

    rest parameters

---
+++ forMySelf/defultParameters/script.js
@@ -0,0 +1,20 @@
+// default parameters
+
+// function addTwo(a, b){
+//     if(typeof b === "undefined"){
+//         b = 15;
+//     }
+// return a + b;
+// }
+// const answer = addTwo(4,5);
+// const answer = addTwo(4);
+// console.log(answer);
+
+
+// function addTwo(a, b = 0){ // this is defult parameters witch is b = 0
+// return a + b;
+// }
+
+// // const answer = addTwo(4,5);
+// const answer = addTwo(4);
+// console.log(answer);
---
+++ forMySelf/restParameters/script.js
@@ -0,0 +1,37 @@
+// rest parameters
+// function addNumbers(a,b,...c){
+// console.log(`a is ${a} and b is ${b}  and c is ${c}`);
+// console.log(`c is out put like array`, c);
+// console.log(`c is out put like array`, c[1]); // out is 5
+// }
+// addNumbers(2,4,3,5,6,7);
+
+
+// function addAll(...numbers){
+//     console.log(numbers); // array
+//     console.log(typeof numbers); // array
+//     console.log(Array.isArray(numbers)); // true
+// }
+// addAll(1,2,3,4,5,6,7);
+
+
+// function addAll(...numbers){
+//     let total = 0;
+// for(let number of numbers){
+//     // console.log(total = number);
+//     total = total + number;
+//     // total = number;
+// }
+// return total;
+// }
+// totalNumbers = addAll(1,2,3,4,5,6,7);
+// console.log(totalNumbers);
+
+
+
+// function addAll(...addAllNumbers){
+// console.log(`numbers`,addAllNumbers);
+// console.log(`numbers`,addAllNumbers[0]);
+// console.log(`numbers`,addAllNumbers[1]);
+// }
+// addAll(3,4,5,6,7,8,9);
