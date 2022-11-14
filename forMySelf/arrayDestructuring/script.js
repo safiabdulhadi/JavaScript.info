eb1f1c71d812290d6a1caabdc21b37ddbbd1a920
safiabdulhadi1994 <safiabdulhadi1994@gmail.com>
2022-11-14 10:42:08 +0100

    arrays

---
+++ forMySelf/arrayDestructuring/script.js
@@ -0,0 +1,14 @@
+// array destructuring
+const myArray = ["value1","value2","value3"];
+// console.log(myArray);
+// let value1 = myArray[0];
+// console.log("value of myvalue1",value1);
+// let value2 = myArray[1];
+// console.log("value of myvalue2", value2);
+
+let [myValue1,myValue2,myValue3] = myArray;
+myValue1 = " let check for to change value of";
+
+console.log("value of myvalue1",myValue1);
+console.log("value of myvalue1",myValue2);
+console.log("value of myvalue1",myValue3);
