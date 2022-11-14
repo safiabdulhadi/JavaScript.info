1bdff711c86a3ccdae8af58d917f0045fce22b71
safiabdulhadi1994 <safiabdulhadi1994@gmail.com>
2022-11-14 09:23:51 +0100

    local or global scope

---
+++ forMySelf/scope/script.js
@@ -0,0 +1,42 @@
+// block scope vs function scope
+// {
+//     let firstName = "Abdulhadi";
+//     console.log(firstName); // i can access it inside the bock scope
+// }
+// console.log(firstName); // i can't access it the first name out side the block
+
+
+
+// {
+
+//     let lastName = "SAFI";
+//     console.log(lastName); // i can access it inside the bock scope
+
+// }
+// console.log(lastName); // i can't access it
+
+
+// {
+//     const lastName = "Safi";
+//     console.log(lastName);
+// }
+// lastName = "khan";
+// console.log(lastName);
+
+// if(true){
+//     let firstName = "Abdulhadi";
+//     console.log(firstName);
+
+// }
+
+
+// let firstName = "nawid";
+// function myApp(){
+//     if(true){
+//     let firstName = "Abdulhadi";
+    // var firstName = "Abdulhadi"; // this will do coz its global scope
+//     console.log(firstName);
+// }
+//  console.log(firstName);
+// }
+// myApp();
