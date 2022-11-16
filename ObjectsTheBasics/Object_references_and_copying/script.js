fbb7a63d4d14923efb1cbab644b9a092f889b699
safiabdulhadi1994 <safiabdulhadi1994@gmail.com>
2022-11-16 11:28:37 +0100

    object references

---
+++ ObjectsTheBasics/Object_references_and_copying/script.js
@@ -0,0 +1,36 @@
+// let user = {
+//     name: 'John',
+//     age: 30
+// };
+
+// let clone = {};
+
+// for(let key in user){
+//     clone[key] = user[key];
+// }
+// console.log(clone);
+
+
+
+// let user = {
+//     name: "John",
+//     sizes: {
+//       height: 182,
+//       width: 50
+//     }
+//   };
+
+//   let clone = structuredClone(user);
+//   console.log(clone);
+
+
+let user={
+    name:"aman",
+    rollno:11
+    }
+
+    let newobj={...user}
+
+
+    console.log(newobj);
+
