9241f1bab35dcd432197f3ee9dae900e3fe1813a
safiabdulhadi1994 <safiabdulhadi1994@gmail.com>
2022-11-18 09:11:47 +0100

    arrow function

--- fundamentals/function/arrowFunction/script.js
+++ fundamentals/function/arrowFunction/script.js
@@ -11,3 +11,33 @@
 // }
 // console.log(isEvent(2)); // true
 // console.log(isEvent(3)); // falses
+
+function sum1(n1, n2){
+return n1 + n2;
+}
+
+const r1 = sum1(39, 35);
+console.log(r1);
+
+//2 // function experssion
+const sum2 = function(n1, n2){
+    return n1 + n2;
+
+}
+
+const r2 = sum2(30, 20);
+console.log(r2);
+
+//3
+const sum3 = (n1, n2)=>{
+    return n1 + n2;
+}
+
+const r3 = sum3(10, 20);
+console.log(r3);
+
+//4
+const sum4 = (n1, n2)=> n1 = n2;
+
+const r4 = sum4(20, 20);
+console.log(r4);
