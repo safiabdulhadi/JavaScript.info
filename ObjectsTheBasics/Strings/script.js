291fe93d2efa0c228252ed4610d8ae3179efc0cb
safiabdulhadi1994 <safiabdulhadi1994@gmail.com>
2022-11-24 19:26:30 +0100

    string

--- ObjectsTheBasics/Strings/script.js
+++ ObjectsTheBasics/Strings/script.js
@@ -1,5 +1,24 @@
-function sum(a,b){
-    return a + b;
-}
+// function sum(a,b){
+//     return a + b;
+// }
 
-console.log(`1 + 2  = ${sum(1,2)} `);
+// console.log(`1 + 2  = ${sum(1,2)} `);
+
+// let str1 = "Hello\nWorld"; // two lines using a newline symbol
+// console.log(str1);
+
+// let str2 = `Hello
+// World`;
+// console.log(str2);
+
+// console.log(str1 == str2);
+
+// let str = `Hello `;
+// console.log(str[0]);
+// console.log(str.at[0]);
+
+// console.log('Interface'[0].toLowerCase());
+// console.log('Interface'[0].toUpperCase());
+// let str = 'Widget with id';
+// console.log(str.indexOf('Widget'));
+// console.log(str.indexOf('with'));
