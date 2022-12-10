dbc620dbc65936a78e35f28907b499a6de67a11f
safiabdulhadi1994 <safiabdulhadi1994@gmail.com>
2022-12-10 21:12:29 +0100

    obj key valeus

--- Datatypes/keys-values-entries/script.js
+++ Datatypes/keys-values-entries/script.js
@@ -6,3 +6,24 @@ let user = {
 for(let value of Object.values(user)){
     console.log(value);
 }
+
+
+function sumSalaries(salaires){
+let sum = 0;
+for(let Salary of Object.values(salaires)){
+    sum += Salary;
+}
+return sum;
+}
+
+
+let salaries = {
+    'John': 100,
+    'Pete': 300,
+    'Mary': 400
+};
+
+console.log(sumSalaries(salaries)); // 800
+
+
+
