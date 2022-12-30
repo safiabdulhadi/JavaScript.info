85a3c4740cc74131ced0f1e40078c209de2467ff
safiabdulhadi1994 <safiabdulhadi1994@gmail.com>
2022-12-30 15:19:45 +0100

    iterator

--- Datatypes/iteratorObject/script.js
+++ Datatypes/iteratorObject/script.js
@@ -1,4 +1,4 @@
-let str = "Hello";
+// let str = "Hello";
 
 // does the same as
 // for (let char of str){
@@ -6,11 +6,33 @@ let str = "Hello";
 // }
 
 
+// let iterator = str[Symbol.iterator]();
 
-let iterator = str[Symbol.iterator]();
+// while (true) {
+//   let result = iterator.next();
+//   if (result.done) break;
+//   console.log(result.value); // outputs characters one by one
+// }
 
-while (true) {
-  let result = iterator.next();
-  if (result.done) break;
-  console.log(result.value); // outputs characters one by one
+// 2 ------------ Array.from
+let arrayLike = {
+    0: "Hello",
+    1: "World",
+    length: 2
+  };
+
+  let arr = Array.from(arrayLike); // (*)
+  console.log(arr.pop()); // World (method works)
+console.log(arr);
+
+
+// 3 Array.from method
+let str = "This is for check";
+let str2Array = Array.from(str);
+console.log(str2Array);
+
+let num = "12345";
+for (let i of num) {
+    console.log(i);
 }
+// 1 2 3 4 5
